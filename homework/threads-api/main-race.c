75b3e55145d4c07fe595965256e85fd07e5ebdbb
Ziad Ahmed <dev.ziadahmed@gmail.com>
2026-08-06 21:23:30 +0300

    u

--- homework/threads-api/main-race.c
+++ homework/threads-api/main-race.c
@@ -1,18 +1,27 @@
+#include <pthread.h>
 #include <stdio.h>
 
 #include "common_threads.h"
 
 int balance = 0;
+pthread_mutex_t lock;
 
-void* worker(void* arg) {
-    balance++; // unprotected access 
+void *worker(void *arg)
+{
+    pthread_mutex_lock(&lock);
+    balance++; // unprotected access
+    pthread_mutex_unlock(&lock);
     return NULL;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+    pthread_mutex_init(&lock, NULL);
     pthread_t p;
     Pthread_create(&p, NULL, worker, NULL);
+    pthread_mutex_lock(&lock);
     balance++; // unprotected access
+    pthread_mutex_unlock(&lock);
     Pthread_join(p, NULL);
     return 0;
 }
